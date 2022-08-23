79d8dd0787fb029caa0341eee264a25d11931fb3
Habib <habibleton@gmail.com>
2022-08-24 02:35:35 +1000

    Less Then The Bare Bones
    This shit is barely anything but u gotta get started somewhere innit

---
+++ Assets/Scripts/Tricks/TrickSystem.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrickSystem : ScriptableObject
+{
+    // Start is called before the first frame update
+    [SerializeField] private Trick[] trickQueue;
+
+
+    public void InputTrick(Trick trick)
+    {
+        
+    }
+
+}
