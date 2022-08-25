962bc87ebafbda0cb6b78dd35bf6d21f0ba4a812
Habib <habibleton@gmail.com>
2022-08-26 01:10:38 +1000

    Actually wrote some fucking code this time lmao
    barely anything but these smallll snippets are basically me just noting down the architecture i had in mind, over the weekend im probably gonna commit like 500 lines of code when im actually bothered to fill in the blanks I've made. We chillin

--- Assets/Scripts/Tricks/TrickSystem.cs
+++ Assets/Scripts/Tricks/TrickSystem.cs
@@ -1,16 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class TrickSystem : ScriptableObject
 {
     // Start is called before the first frame update
     [SerializeField] private Trick[] trickQueue;
+    private int depretiation = 0;
 
 
     public void InputTrick(Trick trick)
     {
+        foreach (Trick TRK in trickQueue)
+        {
+            if (TRK != null && TRK == trick)
+            {
+                depretiation++;
+                break;
+            }
+        }
+        AppendTrick(trick);
+        
+        
         
     }
+
+
+
+    private void AppendTrick(Trick trick)
+    {
+        for (int x = 0; x < trickQueue.Length - 1; x++)
+        {
+            trickQueue[x] = trickQueue[x + 1];
+        }
+        trickQueue[trickQueue.Length - 1] = trick;
+        //trickQueue.Append(trick); .append is a function ???????
+    }
+    
+    
 
 }
