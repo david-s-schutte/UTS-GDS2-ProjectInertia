2dc4672931e4aab830c93ae7fd4d088740575cdb
david-s-schutte <david.schutte01@gmail.com>
2022-09-23 23:50:56 +1000

    Tided up title screen and rank screen - it just gives a default c rank

--- Assets/Scripts/Surfer/Player/PlayerAnimationController.cs
+++ Assets/Scripts/Surfer/Player/PlayerAnimationController.cs
@@ -111,7 +111,8 @@ namespace Surfer.Player
         /*TEMP FUNCTIONS - BASIC UNITY AUDIO FOR NOW*/
         private void PlayWalkSFX(InputAction.CallbackContext ctx)
         {
-            if (controller.isGrounded && isPlatforming)
+            if (controller.isGrounded && isPlatforming
+                )
             {
                 if (!playerAudio.isPlaying)
                 {
