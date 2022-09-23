8b275c6a11cd5a2993da70ac42927f09f8a96d8c
david-s-schutte <david.schutte01@gmail.com>
2022-09-23 22:48:47 +1000

    Added music through a temp game object

--- Assets/Scripts/Surfer/Player/PlayerAnimationController.cs
+++ Assets/Scripts/Surfer/Player/PlayerAnimationController.cs
@@ -111,7 +111,7 @@ namespace Surfer.Player
         /*TEMP FUNCTIONS - BASIC UNITY AUDIO FOR NOW*/
         private void PlayWalkSFX(InputAction.CallbackContext ctx)
         {
-            if (controller.isGrounded)
+            if (controller.isGrounded && isPlatforming)
             {
                 if (!playerAudio.isPlaying)
                 {
