cb46ec4fa69fca690b78de267b0e19fdee867701
DelcanProbably <decbot100@gmail.com>
2022-09-23 22:19:59 +1000

    Movement presentable for sprint 3 demo

--- Assets/Scripts/Surfer/DEBUGBasicCameraPivot.cs
+++ Assets/Scripts/Surfer/DEBUGBasicCameraPivot.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class DEBUGBasicCameraPivot : MonoBehaviour
 {
     private void Update() {
-        Vector2 input = new(Input.GetAxis("Cam X"), Input.GetAxis("Cam Y"));
+        Vector2 input = new(Input.GetAxis("Cam X"), -Input.GetAxis("Cam Y"));
         transform.Rotate(input.y, 0, 0, Space.Self  );
         transform.Rotate(0, input.x, 0, Space.World);
     }
