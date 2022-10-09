b51627cedf6656e48a6d45fc4e5a761a5c6a62bc
DelcanProbably <decbot100@gmail.com>
2022-10-09 22:26:12 +1100

    setup for platform player sticking

--- Assets/Scripts/GameplayGimmicks/ObjectRotator.cs
+++ Assets/Scripts/GameplayGimmicks/ObjectRotator.cs
@@ -2,14 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectRotator : MonoBehaviour
+[RequireComponent(typeof(Rigidbody))]
+public class ObjectRotator : StickyPlatform
 {
+    Rigidbody rb;
     [SerializeField] private float rotateSpeed;
     [SerializeField] private Vector3 axesToRotate;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody>();
+    }
 
     void FixedUpdate()
     {
         Vector3 rotation = axesToRotate * rotateSpeed;
-        gameObject.transform.Rotate(rotation * Time.fixedDeltaTime);
+        // gameObject.transform.Rotate(rotation * Time.fixedDeltaTime);
+        rb.MoveRotation(transform.rotation * Quaternion.Euler(rotation * Time.fixedDeltaTime));
     }
 }
