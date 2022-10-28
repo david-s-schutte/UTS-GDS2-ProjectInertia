32429b8a7a8c51837622a1ff13d99c1750a122c5
Francisco Castro <francisco.castro3030@gmail.com>
2022-10-28 22:57:54 +1100

    fixed player controller and starting  animation not running

--- Assets/Scripts/Surfer/Player/PlayerFeedbackController.cs
+++ Assets/Scripts/Surfer/Player/PlayerFeedbackController.cs
@@ -1,12 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Surfer.Player;
 using UnityEngine;
 
 public class PlayerFeedbackController : MonoBehaviour
 {
     static PlayerFeedbackController Instance;
     [SerializeField] Animator animator;
+    [SerializeField] PlayerAnimationController _animationController;
+
     public PlayerController playerController;
+    
+    private static readonly int Intro = Animator.StringToHash("Intro");
+    private static readonly int InputAmount = Animator.StringToHash("InputAmount");
+    private static readonly int InTheAir = Animator.StringToHash("InTheAir");
+    private static readonly int Boarding = Animator.StringToHash("Boarding");
+    private static readonly int Jump = Animator.StringToHash("Jump");
+    private static readonly int Property = Animator.StringToHash("Double Jump");
+    private static readonly int Grinding = Animator.StringToHash("Grinding");
+    private static readonly int Grind = Animator.StringToHash("Grind");
 
     private void Awake() {
         if (!animator) {
@@ -19,47 +32,48 @@ public class PlayerFeedbackController : MonoBehaviour
             Destroy(Instance);
         }
         Instance = this;
+
+        playerController.enabled = false;
+
     }
 
-    public void Update()
+    private void Start()
     {
-        if (animator.GetBool("Intro") == true)
-        {
-            playerController.enabled = false;
-        }
-        if (animator.GetBool("Intro") == false)
+        _animationController.OnPlayerIntroCompleted += (() =>
         {
             playerController.enabled = true;
-        }
+            Instance.animator.SetBool(Intro, false);
+        });
     }
 
+
     public static void UpdateMoveAmount (float inputSqrMagnitude) {
-        Instance.animator.SetFloat("InputAmount", inputSqrMagnitude);
+        Instance.animator.SetFloat(InputAmount, inputSqrMagnitude);
     }
 
     public static void UpdateGrounded(bool isGrounded) {
-        Instance.animator.SetBool("InTheAir", !isGrounded);
+        Instance.animator.SetBool(InTheAir, !isGrounded);
     }
 
     public static void OnChangeMovementMode() {
-        Instance.animator.SetBool("Boarding", PlayerController.IsSurferMode());
+        Instance.animator.SetBool(Boarding, PlayerController.IsSurferMode());
     }
 
     public static void OnJump() {
-        if (!Instance.animator.GetBool("InTheAir"))
-            Instance.animator.SetTrigger("Jump");
-        if (Instance.animator.GetBool("InTheAir"))
-            Instance.animator.SetTrigger("Double Jump");
+        if (!Instance.animator.GetBool(InTheAir))
+            Instance.animator.SetTrigger(Jump);
+        if (Instance.animator.GetBool(InTheAir))
+            Instance.animator.SetTrigger(Property);
     }
 
     public static void SetGrind(bool isGrinding)
     {
         if (isGrinding)
         {
-            if (!Instance.animator.GetBool("Grinding"))
-                Instance.animator.SetTrigger("Grind");
+            if (!Instance.animator.GetBool(Grinding))
+                Instance.animator.SetTrigger(Grind);
         }
-        Instance.animator.SetBool("Grinding", isGrinding);
+        Instance.animator.SetBool(Grinding, isGrinding);
     }
 
 }
